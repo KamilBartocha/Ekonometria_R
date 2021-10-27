c5e5ef139ee77974e12f5d6214aac3a259293eba
Kamil Bartocha <kamilbartocha53@gmail.com>
2021-10-27 11:24:00 +0200

    lab04_regresja wielominaowa, anova

--- lab03.R
+++ lab03.R
@@ -1,5 +1,5 @@
 library(car)
-
+library(ellipse)
 zarthan <- read.table("zarthan.txt")
 View(zarthan)
 attach(zarthan)
@@ -30,3 +30,17 @@ anova(reg2)
 
 reg0<-lm(sales~1,zarthan)
 anova(reg0,reg)
+
+# A - przedział ufności pokrywa \beta_1
+# B - przedział ufności pokrywa \beta_2
+# chcemy P(a iloczyn b) >= 1-\alfa, rozpisując wychodzi
+# \alpha_0 = \frac{\alpha}{2}
+
+?confint
+confint(reg,c("population","income"),level=0.95)
+plot(ellipse(reg,c("population","income"),level=0.9),type="l")
+rect(0.482813482,0.007089742,0.50919647,0.01130842)
+?predict
+predict(reg,data.frame(population=220,income=2500),interval="confidence",level=0.95)
+predict(reg,data.frame(population=c(220,375),income=c(2500,3500)),
+        interval="prediction",level=0.95)
